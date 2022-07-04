dde7c86e535386623f73f5a167ba31d41e57bdef
Ahmed Mansour <ahmed.mansour232120@gmail.com>
2022-07-04 15:15:12 +0200

    remove minimum from discount model

--- grad2/app/Models/DiscountCode.php
+++ grad2/app/Models/DiscountCode.php
@@ -9,7 +9,7 @@ class DiscountCode extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'shop_id', 'code', 'type','value','minimum_requirements_value','starts_at','ends_at'
+        'shop_id', 'code', 'type','value','starts_at','ends_at'
     ];
     protected $dates = ['ends_at','starts_at'];
     public function user(): BelongsTo
